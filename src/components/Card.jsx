57084147c001704af81a8f9b2db39e8e3b138302
Tyler Wang <yunfeihit@gmail.com>
2026-08-17 10:30:27 +0800

    add 3D flipping effect on Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,32 @@
 import './Card.css'
 import cardBackImg from '../assets/imgs/card-back.png'
+import { useState, useEffect } from 'react'
 
 export default function Card({
     url,
     handleClick
 }) {
+    const [flipped, setFlipped] = useState(false)
+
+    useEffect(() => {
+        setFlipped(true);
+    }, [])
 
     return (
-    <section className='card'>
-        <div className='card-front'>
-            <img 
-                src={url} 
-                alt="memory-card"
-                onClick={handleClick}
-            />  
-        </div>
-        <div className='card-back'>
-            <img
-                src={cardBackImg}
-                alt='card-back'
-            ></img>
-        </div>
+    <section className={`card ${flipped ? "flipped" : ""}`}>
+            <div className='card-front'>
+                <img 
+                    src={url} 
+                    alt="memory-card"
+                    onClick={handleClick}
+                />  
+            </div>
+            <div className='card-back'>
+                <img
+                    src={cardBackImg}
+                    alt='card-back'
+                ></img>
+            </div>
     </section>
     )
 }
